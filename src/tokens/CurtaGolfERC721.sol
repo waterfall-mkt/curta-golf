c1dcff314a8cb385a877479703991dcefe86b644
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-10-24 16:15:06 -0400

    feat: ini CurtaGolfERC721

--- src/tokens/CurtaGolfERC721.sol
+++ src/tokens/CurtaGolfERC721.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
+
+import { ERC721TokenReceiver } from "solmate/tokens/ERC721.sol";
+
+abstract contract CurtaGolfERC721 is ERC721TokenReceiver {
+    // -------------------------------------------------------------------------
+    // Events
+    // -------------------------------------------------------------------------
+
+    event Approval(address indexed owner, address indexed spender, uint256 indexed id);
+
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed id);
+}
