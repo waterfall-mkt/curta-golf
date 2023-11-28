e14bf91c6139983c9cf892c617891cab723f5c68
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-28 15:06:52 -0500

    feat: add mock course + solutions

---
+++ src/utils/mock/MockCourse.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+import { ICourse } from "../../interfaces/ICourse.sol";
+
+/// @title A mock Curta Golf Course for testing
+/// @author fiveoutofnine
+contract MockCourse is ICourse {
+    /// @inheritdoc ICourse
+    function name() external pure returns (string memory) {
+        return "Mock Course";
+    }
+
+    /// @inheritdoc ICourse
+    function run(address _target, uint256 _seed) external view returns (uint32) {
+        uint256 start = gasleft();
+
+        // Generate inputs from `_seed`.
+        uint256 a = _seed >> 128;
+        uint256 b = _seed & 0xffffffffffffffffffffffffffffffff;
+
+        // Run solution.
+        uint256 c = IMockCourse(_target).add(a, b);
+        uint256 end = gasleft();
+
+        unchecked {
+            // Verify solution.
+            if (c != a + b) revert IncorrectSolution();
+
+            // Return gas used.
+            return uint32(start - end);
+        }
+    }
+}
+
+/// @title The interface for `MockCourse`, a mock Curta Golf Course for testing
+interface IMockCourse {
+    /// @notice Adds two numbers together.
+    /// @param _a The first number.
+    /// @param _b The second number.
+    /// @return The sum of `_a` and `_b`.
+    function add(uint256 _a, uint256 _b) external pure returns (uint256);
+}
---
+++ src/utils/mock/MockCourseSolution.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+import { IMockCourse } from "./MockCourse.sol";
+
+/// @title An efficient solution to `MockCourse`.
+/// @author fiveoutofnine
+contract MockCourseSolutionEfficient is MockCourse {
+    /// @inheritdoc IMockCourse
+    function add(uint256 _a, uint256 _b) external pure override returns (uint256) {
+        unchecked {
+            return _a + _b;
+        }
+    }
+}
+
+/// @title An inefficient solution to `MockCourse`.
+/// @author fiveoutofnine
+contract MockCourseSolutionInefficient is MockCourse {
+    /// @inheritdoc IMockCourse
+    function add(uint256 _a, uint256 _b) external pure override returns (uint256) {
+        // Waste some gas.
+        for (uint256 i; i < 100; ++i) { }
+
+        return _a + _b;
+    }
+}
