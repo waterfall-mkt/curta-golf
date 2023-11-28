4b271859da692b86ed11bee73c67a37984b44c2f
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-11-28 15:28:12 -0500

    chore: add sample solution bytecodes

--- test/utils/BaseTest.sol
+++ test/utils/BaseTest.sol
@@ -11,13 +11,29 @@ import { IPurityChecker } from "../../src/interfaces/IPurityChecker.sol";
 import { PurityChecker } from "../../src/utils/PurityChecker.sol";
 import { MockCourse } from "../../src/utils/mock/MockCourse.sol";
 
-/// @notice A base test contract for Curta Golf with events, labeled addresses,
-/// and helper functions for testing. When `BaseTest` is deployed, it sets and
-/// labels 3 addresses: `owner` (owner of the `CurtaGolf` deploy), `solver1`,
-/// and `solver2`. Then, in `setUp`, it deploys an instance of `CurtaGolf`,
-/// `MockCourse`, `Par`, `PurityChecker`, and adds `MockCourse` to `CurtaGolf`
-/// as `owner`.
+/// @notice A base test contract for Curta Golf with constants for sample
+/// solutions, events, labeled addresses, and helper functions for testing. When
+/// `BaseTest` is deployed, it sets and labels 3 addresses: `owner` (owner of
+/// the `CurtaGolf` deploy), `solver1`, and `solver2`. Then, in `setUp`, it
+/// deploys an instance of `CurtaGolf`, `MockCourse`, `Par`, `PurityChecker`,
+/// and adds `MockCourse` to `CurtaGolf` as `owner`.
 contract BaseTest is Test {
+    // -------------------------------------------------------------------------
+    // Constants
+    // -------------------------------------------------------------------------
+
+    /// @notice Bytecode of an efficient solution to `MockCourse`.
+    /// @dev The bytecode outputted when `MockCourseSolutionEfficient` is
+    /// compiled with `0.8.21+commit.d9974bed` and `1_000_000` optimizer runs.
+    bytes constant EFFICIENT_SOLUTION =
+        hex"6080604052348015600f57600080fd5b5060a58061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063771602f714602d575b600080fd5b603c6038366004604e565b0190565b60405190815260200160405180910390f35b60008060408385031215606057600080fd5b5050803592602090910135915056fea264697066735822122053508e1f6f437dc11678aec86f624d167eb4ae75e36f622b6bf518e6edd2a99f64736f6c63430008150033";
+
+    /// @notice Bytecode of an inefficient solution to `MockCourse`.
+    /// @dev The bytecode outputted when `MockCourseSolutionInefficient` is
+    /// compiled with `0.8.21+commit.d9974bed` and `1_000_000` optimizer runs.
+    bytes constant INEFFICIENT_SOLUTION =
+        hex"608060405234801561001057600080fd5b50610158806100206000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c8063771602f714610030575b600080fd5b61004361003e366004610086565b610055565b60405190815260200160405180910390f35b6000805b60648110156100725761006b816100d7565b9050610059565b5061007d828461010f565b90505b92915050565b6000806040838503121561009957600080fd5b50508035926020909101359150565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052601160045260246000fd5b60007fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff8203610108576101086100a8565b5060010190565b80820180821115610080576100806100a856fea26469706673582212200d824e76c3f79d5d524f08c7f5df03e9eee461a0664d228713dc578a86c02fc564736f6c63430008150033";
+
     // -------------------------------------------------------------------------
     // `CurtaGolf` events
     // -------------------------------------------------------------------------
@@ -109,14 +125,22 @@ contract BaseTest is Test {
     // Setup
     // -------------------------------------------------------------------------
 
-    /// @notice Sets and labels addresses for `owner`, `solver1`, and `solver2`.
+    /// @notice Sets and labels addresses for `owner`, `solver1`, `solver2`, and
+    /// sets the .
     constructor() {
+        // Set addresses.
         owner = makeAddr("owner");
         solver1 = makeAddr("solver1");
         solver2 = makeAddr("solver2");
         vm.label(owner, "Curta Golf owner");
         vm.label(solver1, "Solver 1");
         vm.label(solver2, "Solver 2");
+
+        // Deploy solutions to `MockCourse`.
+        /* MockCourseSolutionEfficient solutionEfficient = new MockCourseSolutionEfficient();
+        MockCourseSolutionInefficient solutionInefficient = new MockCourseSolutionInefficient();
+        efficientSolution = address(solutionEfficient).code;
+        inefficientSolution = address(solutionInefficient).code; */
     }
 
     /// @notice Deploys an instance of `CurtaGolf`, `MockCourse`, `Par`,
