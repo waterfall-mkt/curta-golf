f0cd628734ceca015ef07f7f4ef8a60f7362c112
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-12-03 21:58:02 -0500

    fix: purity checker

--- src/interfaces/IPurityChecker.sol
+++ src/interfaces/IPurityChecker.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.21;
 
 /// @title The interface for a purity checker
 /// @notice A purity checker checks whether a given contract is pure, i.e. that
-/// it's restricted to a set of instructions/opcodes, by analyzing its bytecode.
+/// it's restricted to a set of instructions/opcodes.
 interface IPurityChecker {
     /// @notice Checks whether the given bytecode `_code` is pure.
     /// @param _code The bytecode to check.
