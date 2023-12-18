e9aa243d537fe965cd9f7a12e0c71d25e88d7c02
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-12-18 14:21:07 -0600

    update deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.21;
 
 import { Script, console } from "forge-std/Script.sol";
+import { LibRLP } from "solady/utils/LibRLP.sol";
 
 import { CurtaGolf } from "src/CurtaGolf.sol";
 import { Par } from "src/Par.sol";
@@ -22,34 +23,22 @@ contract Deploy is Script {
     /// @notice The instance of `PurityChecker` that will be deployed.
     PurityChecker public purityChecker;
 
-    /// @notice The expected address of the deployed `CurtaGolf` contract.
-    address curtaGolfAddress = 0x8936272ebecc127D21BdC0DbD35978DC7bB7F358;
-
-    /// @notice Address of the create2 factory to use
-    address create2Factory = 0x0000000000FFe8B47B3e2130213B802212439497;
+    /// @notice The deployer address for `curta-golf`.
+    address public deployer = 0x5F3146D3D700245E998660dBCAe97DcD7a554c05;
 
     function run() public {
         vm.startBroadcast();
 
-        // Deploy `PurityChecker`.
         purityChecker = new PurityChecker();
 
-        // Deploy `Par`.
+        // We get what the nonce will be after first deploying `Par.sol`.
+        address curtaGolfAddress = LibRLP.computeAddress(deployer, vm.getNonce(deployer) + 1);
+
         par = new Par(curtaGolfAddress);
 
-        // Deploy `CurtaGolf`.
         curtaGolf = new CurtaGolf(par, purityChecker);
 
-        // create2Factory.call(abi.encodeWithSignature(
-        //     "safeCreate2(bytes32,bytes)",
-        //     0x5f3146d3d700245e998660dbcae97dcd7a554c05c8292664421e00010df48664,
-        //     abi.encodePacked(
-        //         type(CurtaGolf).creationCode,
-        //         abi.encode(address(par), address(purityChecker))
-        //     )
-        // ));
-
-        curtaGolf.transferOwnership(0xB6a7803FF52199A4bBBf902d6fb61069d1b1676a);
+        curtaGolf.transferOwnership(0xA85572Cd96f1643458f17340b6f0D6549Af482F5);
 
         vm.stopBroadcast();
     }
